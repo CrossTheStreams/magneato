93285283a79017c74c9365d9ba0205a4b96fe732
Andrew <arhautau@gmail.com>
2013-06-28 21:31:42 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magneato::Application.config.secret_token = 'f7d275bf33c240da52aee61afcb5df86abaa429e05ba63fc4622194af2cf66e10a5ee0db036434df0b4823838a4194d1dd1064fdb400e1eb9cbcdcb58ffa53ac'
