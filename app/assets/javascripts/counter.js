5c0bbd7d08180fa083ad9f83fc3ee99b5d2a5be2
Andrew <arhautau@gmail.com>
2013-06-29 13:26:39 -0700

    persistance via localStorage

--- app/assets/javascripts/counter.js
+++ app/assets/javascripts/counter.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+  init_count = parseInt(localStorage["reps_count"]);
+
+  if (typeof(init_count) == "undefined") {
+    init_count = 0;
+    ensure_reps_count(init_count);
+  }
+  else {
+    ensure_reps_count(init_count) 
+  }
+
 
   channel = dispatcher.subscribe('rep_channel')
 
@@ -10,8 +20,12 @@ $(document).ready(function() {
 });
 
 function update_count (elem) {
-  var init_count = parseInt($(elem).text());
-  new_count = init_count + 1
+  var old_count = parseInt($(elem).text());
+  new_count = old_count + 1
+  localStorage["reps_count"] = new_count;
   return $(elem).text(new_count.toString());
-}
+};
 
+function ensure_reps_count(count) {
+  $("#reps").text(count.toString()).fadeIn()
+};
--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -1,16 +1,20 @@
 class CounterController < ApplicationController
 
   def index
-      
+       
   end
 
   
   def ping_rep
 
     WebsocketRails[:rep_channel].trigger(:rep_new, {:ping => true})  
-
+    
     render :text => "Yo dawg"
 
+  end
+
+  def clear_reps
+    
   end
 
 
