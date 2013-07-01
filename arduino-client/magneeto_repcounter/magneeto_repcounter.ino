ada8a77c0fd76a19badaff39d3172f895b4c3d47
Morgan Redfield <redfieldm@gmail.com>
2013-07-01 10:18:05 -0700

    updating arduino rep counter with working code

--- arduino-client/magneeto_repcounter/magneeto_repcounter.ino
+++ arduino-client/magneeto_repcounter/magneeto_repcounter.ino
@@ -1,23 +1,18 @@
 /*
-  Magneeto Rep Counter and web client
+  Repeating Wifi Web client
  
  This sketch connects to a a web server and makes a request
- using an Arduino Wifi shield. It also connects to an MPU-9150 
- and uses the acceleration values (from only the z axis) to 
- determine when an exercise rep is being done. A post notification
- is sent to a server when a rep is recognized.
+ using an Arduino Wifi shield.
  
  Circuit:
  * Wifi shield attached to pins 10, 11, 12, 13
- * MPU-9150 attached to pins A4, A5, D2 (with 3V3 power and gnd)
  
- Based on WiFi demo
  created 23 April 2012
  modifide 31 May 2012
  by Tom Igoe
  
- Updated 2013 June 29 by Boris Joffe and Morgan Redfield
- 
+ http://arduino.cc/en/Tutorial/WifiWebClientRepeating
+ This code is in the public domain.
  */
 
 #include <SPI.h>
@@ -42,18 +37,13 @@ int status = WL_IDLE_STATUS;
 WiFiClient client;
 
 // server address:
-//char server[] = "www.arduino.cc";
+//char server[] = "hautaulogy.local";
+char server[] = "192.168.1.137";
 //IPAddress server(64,131,82,241);
-char server[] = "192.168.1.124";
-//char serverIP[] = "www.arduino.cc"
-char serverIP[] = "192.168.1.124";
 
-// http://192.168.1.124:3000/counter/ping
-
-//int serverPort = 80;
-int serverPort = 3000;
-int wifiTTL = 3000;
-
+unsigned long lastConnectionTime = 0;           // last time you connected to the server, in milliseconds
+boolean lastConnected = false;                  // state of the connection last time through the main loop
+const unsigned long postingInterval = 5*1000;  // delay between updates, in milliseconds
 
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
@@ -63,17 +53,16 @@ MPU6050 accelgyro;
 
 int16_t ax, ay, az;
 int rep_state;
-int totalReps = 0;
 
-int ready_led = 7;
-boolean rep;
+boolean led_state;
+int led_pin = 9;
 
 void setup() {
   //Initialize serial and wait for port to open:
-  Serial.begin(38400); 
-  /*while (!Serial) {
+  Serial.begin(9600); 
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
-  }*/
+  }
   
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
@@ -90,99 +79,104 @@ void setup() {
     status = WiFi.begin(ssid, pass);
 
     // wait 10 seconds for connection:
-    // future TODO - make this faster
-    delay(wifiTTL);
+    delay(10000);
   } 
   // you're connected now, so print out the status:
   printWifiStatus();
   
-  // Rep Counter and Accel setup
+  // set up for accel
   // join I2C bus (I2Cdev library doesn't do this automatically)
-  Wire.begin();
-  // initialize device
-  Serial.println("Initializing I2C devices...");
-  
-  pinMode(ready_led, OUTPUT);
-  // FIX - program stops here and requires reset to continue successfully (INTERMITTENT)
-  accelgyro.initialize();
+    Wire.begin();
+     // initialize device
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
 
-  // verify connection
-  Serial.println("Testing device connections...");
-  Serial.println(accelgyro.testConnection() ? "MPU9150 connection successful" : "MPU9150 connection failed");
-  Serial.println();
-  
-  //magneeto custom stuff
-  // todo: add in LPF for accel (can I do that on the MPU9150
-  // todo: add in calibration for random placement on weight
-  rep_state = 0;
-  rep = false;
-  digitalWrite(ready_led, HIGH);
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU9150 connection successful" : "MPU9150 connection failed");
+    
+    //magneeto custom stuff
+    // todo: add in LPF for accel (can I do that on the MPU9150
+    rep_state = 0;
+    
+    pinMode(led_pin, OUTPUT);
+    led_state = HIGH;
+    digitalWrite(led_pin, led_state);   
 }
 
 void loop() {
   // read raw accel/gyro measurements from device
   accelgyro.getAcceleration(&ax, &ay, &az);
     
- while (client.available()) {
-   client.flush(); 
-   char c = client.read();
-  }
- 
- /*
-  // time between request sent and response received
-  unsigned long duration = millis() - requestSentTime;
-  char strDuration[5];
-  
-  char str[100] = "POST Request took ";
-  strcat(str, ltoa(duration, strDuration, 10));
-  
-  Serial.println(str);*/
-  
-  // determine if a rep has been performed
   if (rep_state == 0)
   {
-      if (az < 0)
-      {
-          rep_state = 1; 
-      }
+    if (az < 0)
+    {
+        rep_state = 1; 
+    }
   } else if (rep_state == 1)
   {
-      if (az < -6500) {
-          rep_state = 2; 
-       }
-    } else // rep_state = 2
-    {
-        if (az > 0) {
-            rep_state = 1;
-            rep = true;
-            httpRequest();
+    if (az < -6500) {
+        rep_state = 2; 
+    }
+  } else // rep_state = 2
+  {
+    if (az > 0) {
+        rep_state = 1;
+        Serial.println("rep");
+        if(!client.connected()) {
+          httpRequest();
         }
     }
-    
-    if (rep && !client.connected()) {
-      Serial.println("disconnecting");
-      client.stop();
-      rep = false;
-    }  
+  }
+  
+  // if there's incoming data from the net connection.
+  // send it out the serial port.  This is for debugging
+  // purposes only:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if there's no net connection, but there was one last time
+  // through the loop, then stop the client:
+  if (!client.connected() && lastConnected) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    led_state = !led_state;
+    digitalWrite(led_pin, led_state);
+  }
+
+  // if you're not connected, and ten seconds have passed since
+  // your last connection, then connect again and send data:
+  //if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+  //  httpRequest();
+  //}
+  // store the state of the connection for next time through
+  // the loop:
+  lastConnected = client.connected();
 }
 
 // this method makes a HTTP connection to the server:
 void httpRequest() {
   // if there's a successful connection:
-  
-  if (client.connect(server, serverPort)) {
-    Serial.println("Connecting...");
+  if (client.connect(server, 3000)) {
+    Serial.println("connecting...");
     // send the HTTP PUT request:
     client.println("GET /counter/ping HTTP/1.1");
-    //char hostStr[] = "Host: ";
-    //strcat(hostStr, serverIP);
     client.println("Host: 192.168.1.124");
     //client.println(serverIP);
     client.println("Connection: close");
     client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
   } 
   else {
-    Serial.println("connection failed ... disconnecting");
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
+    Serial.println("disconnecting.");
     client.stop();
   }
 }
